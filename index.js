7fbc68e17293b966a0ac60646bb0d7ee94ef69c5
justin <justincgeorge95@gmail.com>
2020-08-31 15:30:58 +0530

    part3 3.7 3.8

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
 
+const app = express();
 app.use(express.json());
+
+app.use(
+  morgan("tiny", {
+    skip: function (req, res) {
+      return req.method === "POST";
+    },
+  })
+);
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time  ms :logbody",
+    {
+      skip: function (req, res) {
+        return req.method !== "POST";
+      },
+    }
+  )
+);
+
 let persons = [
   {
     name: "Arto hellas",
@@ -36,10 +57,13 @@ app.get("/api/persons", (req, res) => {
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const person = persons.filter(
-    (person) => person.id === Number(req.params.id)
-  );
-  res.json(person);
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
 });
 app.delete("/api/persons/:id", (req, res) => {
   persons = persons.filter((person) => person.id !== Number(req.params.id));
@@ -53,6 +77,18 @@ const generateid = () => {
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
+  morgan.token("logbody", function (req, res) {
+    return JSON.stringify(body);
+  });
+
+  if (!body.name || !body.numberid) {
+    return res.status(400).json({ error: "need both name and numebr field" });
+  }
+
+  if (persons.some((person) => person.name === body.name)) {
+    return res.status(400).json({ error: "name must be unique" });
+  }
+
   const person = {
     name: body.name,
     numberid: body.numberid,
@@ -65,5 +101,5 @@ app.post("/api/persons", (req, res) => {
 const PORT = 3001;
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
